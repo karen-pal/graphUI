452976a50c2ea51cb5e88743db3f0112b571c3db
Karen Palacio <karen.palacio.1994@gmail.com>
2023-12-14 01:39:42 -0300

    interesante glitch

--- graph.py
+++ graph.py
@@ -42,17 +42,35 @@ def create_vertical_graph_animation(word_list):
         ax.clear()
         nx.draw(G, pos, labels=nx.get_node_attributes(G, 'label'), node_size=1000, node_color='black', edge_color='black', font_size=24, font_color='white', ax=ax)
         fig.set_facecolor('green')  # Set the background color
-        ax.set_facecolor('green')  # Set the background color
 
 
     # Create animation
     fig, ax = plt.subplots()
-    ax.set_facecolor('#eeefff')
     ani = FuncAnimation(fig, update, frames=len(word_list), interval=1000, repeat=False)
 
     plt.show()
+    plt.close()
 
 # Example usage
-word_list = ["a", "close", "up", "of", "a", "television", "screen", "with", "a", "lot", "of", "different", "pictures"]
-create_vertical_graph_animation(word_list)
+import csv
+csv_file_path = './random_probabilistic_data_sample_from_inside_my_dream,_denoising-diffusion-probabilistic-models.csv'  # Replace with the path to your CSV file
+image_data = []
+prompt_data = []
 
+with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+    csv_reader = csv.reader(csvfile)
+    next(csv_reader)  # Skip the header row if it exists
+
+    for row in csv_reader:
+        if len(row) == 2:  # Ensure the row has both image and prompt data
+            image_data.append(row[0])
+            prompt_data.append(row[1])
+
+create_vertical_graph_animation(prompt_data[:100])
+i = 0
+for prompt in prompt_data:
+    if (i==3):
+        break
+    word_list = prompt.split(" ")#[a"a", "close", "up", "of", "a", "television", "screen", "with", "a", "lot", "of", "different", "pictures"]
+    create_vertical_graph_animation(word_list)
+    i+=1
